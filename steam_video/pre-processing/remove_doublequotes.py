96e89a081cfdaa7dabef3312d0a773964e7e3699
Raaga Pranitha Kolla <raagapranithakolla@Raagas-MacBook-Pro.local>
2020-08-09 19:09:32 -0700

    Add colab file

--- steam_video/pre-processing/remove_doublequotes.py
+++ steam_video/pre-processing/remove_doublequotes.py
@@ -1,7 +1,8 @@
-f = open('/Users/raagapranithakolla/sjsu/cmpe256/homework2/temp.txt','r')
+import os
+f = open(os.getcwd()+'/temp.txt','r')
 lines = f.readlines()
 f.close()
-f1 = open('/Users/raagapranithakolla/sjsu/cmpe256/homework2/video_bundle_data2.json','w')
+f1 = open(os.getcwd()+'/video_bundle_data2.json','w')
 f1.write('[')
 for idx,line in enumerate(lines):
 	if idx == 0:
--- steam_video/pre-processing/string_to_json.py
+++ steam_video/pre-processing/string_to_json.py
@@ -1,35 +1,19 @@
 import json
 import os
-# f = open('/Users/raagapranithakolla/Downloads/bundle_data.json','r')
-# data = f.read()
-# f.close()
-# f2 = open('/Users/raagapranithakolla/sjsu/cmpe256/homework2/temp.txt','w')
-# f2.write((data))
-# f2.close()
+f = open(os.getcwd()+'/bundle_data.json','r')
+data = f.read()
+f.close()
+f2 = open(os.getcwd()+'/temp.txt','w')
+f2.write((data))
+f2.close()
 
-f = open('/Users/raagapranithakolla/sjsu/cmpe256/homework2/temp.txt','r')
+f = open(os.getcwd()+'/temp.txt','r')
 lines = f.readlines()
 f.close()
-f1 = open('/Users/raagapranithakolla/sjsu/cmpe256/homework2/temp1.txt','w')
+f1 = open(os.getcwd()+'/temp1.txt','w')
 f1.write('[')
 for line in lines:
 	temp = line+','
-	# temp = temp.replace("'b",'"b')
 	f1.writelines((temp))
 f1.write(']')
-f1.close()
-# import json
-# import ast
-
-# fr=open("/Users/raagapranithakolla/Downloads/bundle_data.json")
-# fw=open("/Users/raagapranithakolla/sjsu/cmpe256/homework2/video_bundle_data.json", "w")
-
-# for line in fr:
-#     json_dat = json.dumps(ast.literal_eval(str(line)))
-#     dict_dat = json.loads(json_dat)
-#     json.dump(dict_dat, fw)
-#     fw.write("\n")
-
-# fw.close()
-# fr.close()
-# 	
+f1.close() 	
